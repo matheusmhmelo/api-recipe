08b52ab27975ff7d895c5fb441b174565f03d705
matheusmelo <ma_hrm@hotmail.com>
2020-11-26 15:50:09 -0300

    Tests on formatting ingredients

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.14
 
 require (
 	github.com/gorilla/mux v1.8.0
+    github.com/stretchr/testify v1.3.0
 	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776
 )
---
+++ internal/services/recipes/format_test.go
@@ -0,0 +1,76 @@
+package recipes
+
+import (
+	"errors"
+	"github.com/stretchr/testify/assert"
+	"strings"
+	"testing"
+)
+
+func Test_format(t *testing.T) {
+	tests := []struct {
+		name string
+		s    string
+		want []string
+		err  error
+	}{
+		{
+			"[format] Normal ingredients",
+			"onions,garlic",
+			[]string{"onions","garlic"},
+			nil,
+		},
+		{
+			"[format] Special characters ingredients",
+			"onions_,^garlic=",
+			[]string{"onions","garlic"},
+			nil,
+		},
+		{
+			"[format] Limit of ingredients reached",
+			"onions,garlic,pepper,salsa",
+			[]string(nil),
+			errors.New("ingredient limit reached, choose only 3"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := format(tt.s)
+			assert.Equal(t, got, tt.want)
+
+			if tt.err != nil {
+				if err == nil {
+					t.Errorf("expected error: %v", tt.err.Error())
+				} else if !strings.Contains(err.Error(), tt.err.Error()) {
+					t.Errorf("unexpected error: %v", err)
+				}
+			}
+		})
+	}
+}
+
+
+func Test_formatIngredients(t *testing.T) {
+	tests := []struct {
+		name string
+		s    string
+		want []string
+	}{
+		{
+			"[formatIngredients] Ingredients from API",
+			"onions, garlic, pepper, salsa",
+			[]string{"garlic","onions","pepper","salsa"},
+		},
+		{
+			"[formatIngredients] Ingredients without format of API",
+			"onions,garlic,pepper,salsa",
+			[]string{"onions,garlic,pepper,salsa"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := formatIngredients(tt.s)
+			assert.Equal(t, got, tt.want)
+		})
+	}
+}
